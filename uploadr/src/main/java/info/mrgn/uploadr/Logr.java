59c2cbae8c05bde8d41af06ac43b6d556bd61f01
Shibu <shibu@connectfinancials.com>
2018-11-05 00:49:22 +0530

    making logr static - bug cleared

--- uploadr/src/main/java/info/mrgn/uploadr/Logr.java
+++ uploadr/src/main/java/info/mrgn/uploadr/Logr.java
@@ -3,7 +3,7 @@ package info.mrgn.uploadr;
 import android.util.Log;
 
 public class Logr {
-    public void log(String tag, String message){
+    public static void log(String tag, String message){
         Log.d(tag, message);
     }
 }
